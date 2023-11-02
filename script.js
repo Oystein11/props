2e669124364f734bfc933d08a46c7518c56f5cdc
Øystein <oystein.koder@gmail.com>
2023-11-02 10:51:25 +0100

    changes

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 // Import for dark mode fra en annen script
 import {theme_toggle} from "./theme_toggle.js";
 
-//Verdier for hamburgermeny
-const navBar = document.querySelector (".nav_bar_ID");
-const hamburger = document.querySelector (".hamburger_ID");
-
+// html elements we need to use javascript on
+const nav_bar = document.querySelector(".nav_bar_ID");
+const menu= document.querySelector(".hamburger");
+// tracks if the navbar is opened or not
 let menu_open = false;
 
-// Funksjon for åpning og lukking av hamburgermeny
-function toggle_menu () {
-    if (menu_open === false) {
-        navBar.classList = "showNav";
-        hamburger.classList = "open_menu";
-        menu_open = true;
-    } else {
-        navBar.classList = "nav_bar_ID";
-        hamburger.classList = "hamburger_ID";
-        menu_open = false;
-    }
+// toggles navbar and changes the styling
+function toggle_nav() {
+  if (menu_open === false) {
+    nav_bar.classList = "show_nav";
+    menu.classList = "open_menu";
+    menu_open = true;
+  } else {
+    nav_bar.classList = "nav_bar_ID";
+    menu.classList = "hamburger";
+    menu_open = false;
+  }
 }
 
 // Knapp for dark mode
